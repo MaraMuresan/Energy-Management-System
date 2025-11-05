5418c068306af403b965d3cdc7a0c51ff9cab40a
Mara <mara.muresan2003@gmail.com>
2025-11-05 17:24:15 +0200

    feat(authentication-microservice): initial commit

--- authentication-microservice/src/test/java/com/example/authenticationmicroservice/AuthenticationMicroserviceApplicationTests.java
+++ authentication-microservice/src/test/java/com/example/authenticationmicroservice/AuthenticationMicroserviceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.devicemanagementmicroservice;
+package com.example.authenticationmicroservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DeviceManagementMicroserviceApplicationTests {
+class AuthenticationMicroserviceApplicationTests {
 
     @Test
     void contextLoads() {
